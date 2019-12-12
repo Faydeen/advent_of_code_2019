98d8a38746985bc39b6af3d50a481a9987fc7f20
Simon Anché <anche.simon@gmail.com>
2019-12-12 19:48:20 +0100

    day 12 done!

--- day12.py
+++ day12.py
@@ -7,9 +7,19 @@ class Vector(object):
         self.y = y
         self.z = z
 
+    def __repr__(self):
+        return f"<x= {self.x} y= {self.y} z= {self.z} >"
+
+    def __str__(self):
+        return f"<x= {self.x} y= {self.y} z= {self.z} >"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
 
 class Planet(object):
-    def __init__(self, x, y, z):
+    def __init__(self, id,  x, y, z):
+        self.id = id
         self.position = Vector(x, y, z)
         self.velocite = Vector(0, 0, 0)
         self.potential_energy = 0
@@ -22,8 +32,8 @@ class Planet(object):
             self.position.x) + abs(self.position.y) + abs(self.position.z)
 
     def updateKineticEnergy(self):
-        self.kinetic_energy = abs(self.position.x) + \
-            abs(self.position.y) + abs(self.position.z)
+        self.kinetic_energy = abs(self.velocite.x) + \
+            abs(self.velocite.y) + abs(self.velocite.z)
 
     def updatePosition(self):
         self.position.x += self.velocite.x
@@ -32,9 +42,9 @@ class Planet(object):
         self.updatePotentialEnergy()
 
     def updateVelocite(self, x, y, z):
-        dx = 1
-        dy = 1
-        dz = 1
+        dx = -1
+        dy = -1
+        dz = -1
         if self.position.x < x:
             dx = 1
         elif self.position.x == x:
@@ -52,14 +62,28 @@ class Planet(object):
         self.velocite.z += dz * 1
         self.updateKineticEnergy()
 
+    def get_total_energy(self):
+        return self.kinetic_energy * self.potential_energy
+
+    def __str__(self):
+        return f"{self.position} {self.velocite}"
+
+    def __repr__(self):
+        return f"self.id: \n\t{self.position}\n\t{self.velocite}"
+
+    def __eq__(self, other):
+        return self.velocite == other.velocite and self.position == other.position
+
 
 def parse(lines):
     planets = []
+    planetsName = ["Io", "Europa", "Ganymede", "Callisto"]
     for line in lines:
         x = int(line.split("x=")[1].split(", ")[0])
         y = int(line.split("y=")[1].split(", ")[0])
         z = int(line.split("z=")[1].split(">")[0])
-        planets.append(Planet(x, y, z))
+        planets.append(Planet(planetsName[0], x, y, z))
+        planetsName = planetsName[1:]
     return planets
 
 
@@ -67,14 +91,66 @@ def updateVelocity(p1, p2):
     p1.updateVelocite(p2.position.x, p2.position.y, p2.position.z)
 
 
+def simulate(planets):
+    # Update velocity
+    for pair in itertools.permutations(planets, 2):
+        updateVelocity(*pair)
+    # Update position
+    for planet in planets:
+        planet.updatePosition()
+
+
 def solve(planets, nbStep):
     all_pairs = itertools.permutations(planets, 2)
-    for step in range(nbStep + 1):
-        # Update velocity
-        for pair in all_pairs:
-            updateVelocity(*pair)
-        # Update position
+    print(f"After 0 step:")
+    for planet in planets:
+        print(planet)
+    for step in range(1, nbStep + 1):
+        simulate(planets)
+        print(f"After {step} step:")
         for planet in planets:
-            planet.updatePosition()
-        print(f'Step: {step} planets : {planets}')
-    return sum(map(lambda p: p.potential_energy, planets))
+            print(planet)
+    return sum(map(lambda p: p.get_total_energy(), planets))
+
+
+def solve2(planets):
+    steps = 1
+    initial_x_proj = [planet.position.x for planet in planets]
+    initial_y_proj = [planet.position.y for planet in planets]
+    initial_z_proj = [planet.position.z for planet in planets]
+    x_sol = None
+    y_sol = None
+    z_sol = None
+    while x_sol == None or y_sol == None or z_sol == None:
+        simulate(planets)
+        y_proj = [planet.position.y for planet in planets]
+        z_proj = [planet.position.z for planet in planets]
+        if x_sol == None:
+            pos_proj = [planet.position.x for planet in planets]
+            vel_proj = [planet.velocite.x for planet in planets]
+            if pos_proj == initial_x_proj and vel_proj == [0, 0, 0, 0]:
+                x_sol = steps
+        if y_sol == None:
+            pos_proj = [planet.position.y for planet in planets]
+            vel_proj = [planet.velocite.y for planet in planets]
+            if pos_proj == initial_y_proj and vel_proj == [0, 0, 0, 0]:
+                y_sol = steps
+        if z_sol == None:
+            pos_proj = [planet.position.z for planet in planets]
+            vel_proj = [planet.velocite.z for planet in planets]
+            if pos_proj == initial_z_proj and vel_proj == [0, 0, 0, 0]:
+                z_sol = steps
+        steps += 1
+    return ppcm(x_sol, y_sol, z_sol)
+
+
+def ppcm(*n):
+    """Calcul du 'Plus Petit Commun Multiple' de n (>=2) valeurs entières (Euclide)"""
+    def _pgcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+    p = abs(n[0]*n[1])//_pgcd(n[0], n[1])
+    for x in n[2:]:
+        p = abs(p*x)//_pgcd(p, x)
+    return p
--- day12_test.py
+++ day12_test.py
@@ -7,15 +7,32 @@ class TestBasic(unittest.TestCase):
     def test_pass(self):
         data = puzzle.parse(read_file("12", "1"))
         result = puzzle.solve(data, 1000)
-        print(f"Solution: {result}")
-        self.assertEqual(True, True)
+        self.assertEqual(result, 9876)
+
+    def test_pass2(self):
+        data = puzzle.parse(read_file("12", "1"))
+        result = puzzle.solve2(data)
+        self.assertEqual(result, 307043147758488)
 
 
 class TestUnitaire(unittest.TestCase):
-    def testAdd(self):
-        # data = [1, 0, 0, 0, 99]
-        # answer = puzzle.solve(data, 10)
-        self.assertEqual(True, True)
+    def testExample(self):
+        data = puzzle.parse(["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>",
+                             "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"])
+        answer = puzzle.solve(data, 10)
+        self.assertEqual(answer, 179)
+
+    def testExample2(self):
+        data = puzzle.parse(["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>",
+                             "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"])
+        answer = puzzle.solve2(data)
+        self.assertEqual(answer, 2772)
+
+    def testExample3(self):
+        data = puzzle.parse(
+            ["<x=-8, y=-10, z=0>", "<x=5, y=5, z=10>", "<x=2, y=-7, z=3>", "<x=9, y=-8, z=-3>"])
+        answer = puzzle.solve2(data)
+        self.assertEqual(answer, 4686774924)
 
 
 if __name__ == '__main__':
